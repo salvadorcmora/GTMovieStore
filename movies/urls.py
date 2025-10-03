51996df8df5715753e82b9c68feeffde61459182
Salvador Mora <151172513+salvadorcmora@users.noreply.github.com>
2025-10-03 01:19:41 -0400

    Update urls.py
    Added Petitions

--- movies/urls.py
+++ movies/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('favorites/', views.favorites, name='movies.favorites'),
     path('<int:id>/toggle-favorite/', views.toggle_favorite, name='movies.toggle_favorite'),
     path('<int:id>/reviews/<int:review_id>/report/', views.report_review, name='movies.report_review'),
+    path("petitions/", views.petitions_list, name="movies.petitions_list"),
+    path("petitions/new/", views.petitions_create, name="movies.petitions_create"),
+    path("petitions/<int:id>/vote/", views.petition_vote_yes, name="movies.petition_vote_yes"),
+    
 ]
 
